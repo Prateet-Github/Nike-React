2f81a174ea25f05f43405d583f987d9dbaabd313
Prateet Tiwari <prateettiwari29@gmail.com>
2024-12-13 01:28:29 +0530

    initial commit

---
+++ src/Sections/Subscribe.jsx
@@ -0,0 +1,26 @@
+import Button from "../Components/Button";
+
+const Subscribe = () => {
+  return (
+    <section
+      className="max-container flex justify-between items-center max-1g: flex-col gap-10"
+      id="contact-us"
+    >
+      <h3 className="text-4xl leading-[68px] lg:max-w-md font-palanquin font-bold ">
+        Sign Up For<span className="text-coral-red"> Updates</span> & Newsletter
+      </h3>
+      <div className="flex justify-between items-center gap-2">
+        <input
+          type="text"
+          placeholder="Subscribe @nike.com"
+          className="input border-2 border-coral-red border-solid rounded"
+        ></input>
+        <div>
+          <Button label="Sign Up" ></Button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Subscribe;
---
+++ src/Sections/index.js
@@ -0,0 +1,20 @@
+import Hero from "./Hero";
+import Footer from "./Footer";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+import SpecialOffers from "./SpecialOffers";
+import SuperQuality from "./SuperQuality";
+import CustomerReviews from "./CustomerReviews";
+import Subscribe from "./Subscribe";
+
+export{
+  Hero,
+  Footer,
+  PopularProducts,
+  Services,
+  SpecialOffers,
+  SuperQuality,
+  CustomerReviews,
+  Subscribe,
+
+}
